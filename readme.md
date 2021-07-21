c8fd50cf50679a1ac37260652ec889a7c7c3f159
Hubert MUGABO <mugberto@gmail.com>
2021-07-21 15:19:05 +0200

    Add stringLength function and test

---
+++ readme.md
@@ -0,0 +1,1 @@
+Jest framework testing practice
---
+++ stringFunction.test.js
@@ -0,0 +1,4 @@
+const { test, expect } = require('@jest/globals');
+const stringLength = require('./stringFunction');
+
+test('Length of \'test\' to be 4', () => { expect(stringLength('test')).toBe(4); });
